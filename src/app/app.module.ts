1666ef5984a00f9adc2908bb9601eb3d0c092882
judaramburo <judaramburo@gmail.com>
2020-06-19 17:16:31 -0500

    Create service and install bootstrap with npm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,29 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { DirectiveComponent } from './directive/directive.component';
+import { ClientsComponent } from './clients/clients.component';
+import { ClientService } from './clients/client.service';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/clients', pathMatch: 'full' },
+  { path: 'directives', component: DirectiveComponent },
+  { path: 'clients', component: ClientsComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    DirectiveComponent
+    DirectiveComponent,
+    ClientsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [ClientService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
